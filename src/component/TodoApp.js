bfaa428aa317abbe2f85ad6390a7c5ff19e5abf8
elmehdimouhajer <elmehdimouhajer@gmail.com>
2024-09-01 22:55:58 +0100

    Add TodoApp, TodoForm, TodoItem, TodoList

---
+++ src/component/TodoApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, completed: false }];
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} removeTodo={removeTodo} />
+    </div>
+  );
+};
+
+export default TodoApp;
